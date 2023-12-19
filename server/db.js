e537ec8b766ee888752c5b5b7bc5d507cdd0f016
AnowarYusif <sadat.yusif@icloud.com>
2023-12-19 09:46:46 +0500

    Created get route to query database

--- server/db.js
+++ server/db.js
@@ -6,3 +6,19 @@ database: 'favlinks',
 password: 'admin',
 port: 5432,
 })
+
+const getLinks = async (req, res) => {
+    try {
+      const result = await pool.query('SELECT * FROM favlinks ORDER BY id ASC');
+      res.status(200).json(result.rows);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  };
+  
+  
+  module.exports = {
+    getLinks,
+  };
+  
